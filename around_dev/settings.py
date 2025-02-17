e12493f9dcfa37cfac6032e8ee1a4a28782c4a00
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-02-17 11:17:53 +0100

    Configure template directory and set up base URL route

--- around_dev/settings.py
+++ around_dev/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'around_dev.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'around_dev/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- around_dev/urls.py
+++ around_dev/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from landing_page import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.base, name='base'),
 ]
