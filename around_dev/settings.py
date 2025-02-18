92ff02e9d48055ee07e62c739125495b28dd21fc
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-02-18 16:15:10 +0100

    Add hero section and styling

--- around_dev/settings.py
+++ around_dev/settings.py
@@ -125,8 +125,9 @@ STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'static'),
 ]
 
-# Enable WhiteNoise to serve static files 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Enable WhiteNoise to serve static files in production
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
