d71732d6213ce1428c29f3698ec9afd90e3ec2ea
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-02-18 07:23:32 +0100

    Configure production settings and add Procfile for Heroku deployment

--- around_dev/settings.py
+++ around_dev/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tb^u&t-8r05$g3w)w5(pox)vb20eemvdt^p(8fgl2*s840vpk7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn around_dev.wsgi
