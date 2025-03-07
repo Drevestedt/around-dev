7cc9834dcee8beadd7a8b7618663a34ac2baeedc
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-03-07 22:42:35 +0100

    Start adding featured projects on landing page

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from portfolio.models import Project
 
 def base(request):
   return render(request, 'base.html')
 
 def home(request):
-  
+  # Display 3 projects on home page 
+  # projects = Project.objects.all()[3]
   return render(request, 'home.html', {})
