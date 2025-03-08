38236c478be56080ffa27fa3083feb81a10dc5f6
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-03-08 22:13:16 +0100

    Add secret key

--- around_dev/settings.py
+++ around_dev/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os 
+import os
+import environ
 from pathlib import Path
 import dj_database_url 
 if os.path.isfile("env.py"):
@@ -25,10 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tb^u&t-8r05$g3w)w5(pox)vb20eemvdt^p(8fgl2*s840vpk7'
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
