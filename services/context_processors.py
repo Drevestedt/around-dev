07a9e093bb019efd0b5de55dddc6b3641f40238b
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-04-12 22:04:48 +0200

    Refactor cart item count display logic for improved clarity

--- services/context_processors.py
+++ services/context_processors.py
@@ -1,10 +1,14 @@
 from .models import Cart
 
 def cart_item_count(request):
-  if request.user.is_authenticated:
-    cart = Cart.objects.filter(user=request.user).first()
-  else:
-    cart = Cart.objects.filter(user=None).first()
-  if cart:
-    count = cart.services.count()
+  count = 0
+  try:
+    if request.user.is_authenticated:
+      cart = Cart.objects.filter(user=request.user).first()
+    else:
+      cart = Cart.objects.filter(user=None).first()
+    if cart:
+      count = cart.services.count()
+  except:
+    count = 0
   return {'cart_item_count': count}
