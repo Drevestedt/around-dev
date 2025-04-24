f06c6eae132e0253a3f8f0ed928ae2ff51325424
Patrik Drevestedt <patrik.drevestedt@me.com>
2025-04-24 02:28:18 +0200

    Start adding services to dashboard

--- user_account/urls.py
+++ user_account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.user_account, name='user_account'),
+  path('add_services_to_account/', views.add_services_to_account, name='add_services_to_account')
 ]
--- user_account/views.py
+++ user_account/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def user_account(request):
+  services = request.session.get('services', [])
   context = {
-    'dashboard': 'Dashboard content goes here'
+    'services': services
   }
-
   return render(request, 'user_account.html', context)
+
+
+def add_services_to_account(request):
+  if request.method == 'POST':
+    service = request.POST.get('service')
+    rate = request.POST.get('rate')
+
+    if 'services' not in request.session: #TODO: Change to store in a model instead of session
+      request.session['services'] = []
+
+    request.session['services'].append({'service': service, 'rate': rate})
+    request.session.modified = True
+
+  return redirect('user_account')
